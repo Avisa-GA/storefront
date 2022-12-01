17f10a6d810d2f3c3fdaf50cdfb07864e1cdd5c9
Avisa Poshtkouhi <aap571420@gmail.com>
2022-11-30 16:23:36 -0800

    fix: circular rel

--- store/models.py
+++ store/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 
+
+class Promotion(models.Model):
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
 # Create your models here.
 class Collection(models.Model):
     title = models.CharField(max_length=255)
+    featured_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, related_name='+')
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
@@ -11,6 +16,7 @@ class Product(models.Model):
     inventory = models.IntegerField()
     last_updated = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
+    promotions = models.ManyToManyField(Promotion)
 
 class Customer(models.Model):
     MEMBERSHIP_BRONZE = 'B'
